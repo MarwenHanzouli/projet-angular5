9175a2def676d1421221fa9329ed648305e52b0e
marwen hanzouli <marwenhanzouli@gmail.com>
2019-10-04 00:09:12 +0100

    create an observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AppareilService } from './services/appareil.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-  constructor(private appareilService: AppareilService) {
+export class AppComponent implements OnInit{
 
+  secondes: number;
+
+  ngOnInit (){
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
   }
 }
