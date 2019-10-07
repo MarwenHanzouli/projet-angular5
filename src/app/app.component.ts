4be469a4bfcd0fa90c114e2aeb0f1d9a856dbb22
marwen hanzouli <marwenhanzouli@gmail.com>
2019-10-07 15:36:37 +0100

    import subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppareilService } from './services/appareil.service';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/observable/interval';
 
 @Component({
@@ -11,7 +12,7 @@ import 'rxjs/add/observable/interval';
 export class AppComponent implements OnInit, OnDestroy{
 
   secondes: number;
-
+  counterSubscription : Subscription;
   ngOnInit (){
     const counter = Observable.interval(1000);
     this.counterSubscription = counter.subscribe(
