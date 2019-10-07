ce39c745880d3fdedde45ec23c3d105c404ce8e0
marwen hanzouli <marwenhanzouli@gmail.com>
2019-10-07 18:01:50 +0100

    from

---
+++ src/app/edit-appareil-component/edit-appareil-component.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-edit-appareil-component',
+  templateUrl: './edit-appareil-component.component.html',
+  styleUrls: ['./edit-appareil-component.component.css']
+})
+export class EditAppareilComponentComponent implements OnInit {
+
+  defaultOnOff = 'éteint';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
+}
